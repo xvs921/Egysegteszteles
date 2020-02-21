64b292d33859c58b7540d7d2c8528d9ea9aa847f
VorakGergo <vorakgeri@gmail.com>
2020-02-21 10:06:09 +0100

    kesz feladat

---
+++ Egysegteszteles/Repulo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egysegteszteles
+{
+    class Repulo
+    {
+        int ferohelyek;
+        int foglalas;
+        private int v;
+
+        public Repulo(int ferohelyek)
+        {
+            if (ferohelyek<=0)
+            {
+                throw new ArgumentException("A férőhelyek száma pozitív kell legyen");
+            }
+            this.ferohelyek = ferohelyek;
+        }
+
+        public double SzabadHelyek {
+            get { return ferohelyek- foglalas; }
+        }
+
+        public bool teli
+        {
+            get
+            {
+                return ferohelyek == foglalas;
+            }
+        }
+
+        public bool lefoglal()
+        {
+            if (teli)
+            {
+                return false;
+            }
+            foglalas++;
+            return true;
+        }
+
+    }
+}
---
+++ Egysegteszteles/tests/RepuloTest.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egysegteszteles.tests
+{
+    [TestFixture]
+    class RepuloTest
+    {
+        Repulo repulo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repulo = new Repulo(32);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            //....
+        }
+
+        [TestCase]
+        public void UjRepulonMindenHelySzabad()
+        {
+            Assert.AreEqual(32, repulo.SzabadHelyek, "A férőhelyek száma hibás!");
+
+            var repulo2 = new Repulo(86);
+            Assert.AreEqual(32, repulo.SzabadHelyek, "A férőhelyek száma hibás!");
+        }
+        [TestCase]
+        public void HelyfoglalasUtanAHelyekSzamaCsokken()
+        {
+            repulo.lefoglal();
+            Assert.AreEqual(31, repulo.SzabadHelyek, "Hibás számítás a maradék férőhelyeknél!");
+        }
+        [TestCase]
+        public void RepuloTeli()
+        {
+            Assert.IsFalse(repulo.teli, "Üres repülő teli van");
+            for (int i = 0; i < 32; i++)
+            {
+                repulo.lefoglal();
+            }
+            Assert.IsTrue(repulo.teli, "Teli repulo megsincs tele");
+        }
+        [TestCase]
+        public void teliRepuloreNeLehessenHelyetFoglalni()
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                repulo.lefoglal();
+            }
+
+            bool sikerult=repulo.lefoglal();
+            Assert.AreEqual(0, repulo.SzabadHelyek);
+            Assert.IsTrue(repulo.teli);
+            Assert.IsFalse(sikerult);
+        }
+
+        void NegativRepuloLetrehozas()
+        {
+            var rep = new Repulo(-2);
+        }
+
+        [TestCase]
+        public void AHelyekSzamaNemLehetNegativ()
+        {
+            Assert.Throws<ArgumentException>(NegativRepuloLetrehozas);
+            //alternatív mód->lambda függvény
+            Assert.Throws<ArgumentException>(() => {
+                var rep = new Repulo(-2);
+            });
+        }
+
+    }
+}
